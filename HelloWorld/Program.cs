bd1734081cbce1d512dc4b05bb3ee10546d05479
sadamcik <stadas@stadas.sk>
2016-10-12 12:03:37 +0200

    OK

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!!");
+            Console.WriteLine("Hello world!!! Ok");
         }
     }
 }
